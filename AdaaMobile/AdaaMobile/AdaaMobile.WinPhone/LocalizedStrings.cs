5c63e13cbeb93a8908940a36088ffd8e5f515765
Mostafa El-Abady <mostafa.elabady@gmail.com>
2015-10-15 13:46:55 +0200

    Initial Code

---
+++ AdaaMobile/AdaaMobile/AdaaMobile.WinPhone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using AdaaMobile.WinPhone.Resources;
+
+namespace AdaaMobile.WinPhone
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
