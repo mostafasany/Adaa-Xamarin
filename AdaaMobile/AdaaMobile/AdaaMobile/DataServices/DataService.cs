4f006d9fb0031ef5e9dec3c216565eff0af31790
ahmedrashad <ahmedrashad.1990@hotmail.com>
2015-10-21 23:36:52 +0200

    -Added Auto fac library for more advanced IOC. -Addec Empty ViewModelLocator and Data Service.

---
+++ AdaaMobile/AdaaMobile/AdaaMobile/DataServices/DataService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdaaMobile.Models;
+
+namespace AdaaMobile.DataServices
+{
+    public class DataService : IDataService
+    {
+        public Task<User> LoginAsync(string userName, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
