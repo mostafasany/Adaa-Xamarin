730c1d3e224275c764f30e97e59a10577b7e2c0c
Mostafa <mostafa.elabady@gmail.com>
2015-11-12 13:09:43 +0200

    Updated App.cs

---
+++ AdaaMobile/AdaaMobile/AdaaMobile/App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AdaaMobile.ViewModels;
+using AdaaMobile.Views;
+using Xamarin.Forms;
+
+namespace AdaaMobile
+{
+	public partial class App : Application
+	{
+		public App(Locator locator)
+		{
+			//Build dependencies and set static instance of Container
+			Locator.Container = locator.CreateContainer();
+			//Set static instance of locator.
+			Locator.Default = locator;
+
+			// The root page of your application
+			MainPage = new NavigationPage(new LoginPage());
+		}
+
+		protected override void OnStart()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
