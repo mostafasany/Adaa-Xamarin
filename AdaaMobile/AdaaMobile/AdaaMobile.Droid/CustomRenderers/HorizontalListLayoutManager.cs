fb59411cbf24e18780162b341a08ae929525a4d4
ahmedrashad <ahmedrashad.1990@hotmail.com>
2015-11-30 02:03:50 +0200

    -Fixed Wrap content issue in Horizontal List View.

---
+++ AdaaMobile/AdaaMobile/AdaaMobile.Droid/CustomRenderers/HorizontalListLayoutManager.cs
@@ -0,0 +1,112 @@
+using Android.Content;
+using Android.Support.V7.Widget;
+using Android.Views;
+
+namespace AdaaMobile.Droid.CustomRenderers
+{
+    //Ported from this Android Answer http://stackoverflow.com/a/28303955/2086772
+    //It solves Wrap_Content issue for Recycler Viewer for vertical and horiztonal orientations.
+    public class HorizontalListLayoutManager : LinearLayoutManager
+    {
+
+        public HorizontalListLayoutManager(Context context, int orientation, bool reverseLayout) :
+            base(context, orientation, reverseLayout)
+        {
+
+        }
+
+        private int[] _mMeasuredDimension = new int[2];
+
+        public int GetMeasuredWidth()
+        {
+            return _mMeasuredDimension[0];
+        }
+
+        public int GetMeasuredHeight()
+        {
+            return _mMeasuredDimension[1];
+        }
+
+        public override void OnMeasure(RecyclerView.Recycler recycler, RecyclerView.State state,
+                              int widthSpec, int heightSpec)
+        {
+            MeasureSpecMode widthMode = View.MeasureSpec.GetMode(widthSpec);
+            MeasureSpecMode heightMode = View.MeasureSpec.GetMode(heightSpec);
+            int widthSize = View.MeasureSpec.GetSize(widthSpec);
+            int heightSize = View.MeasureSpec.GetSize(heightSpec);
+            int width = 0;
+            int height = 0;
+            for (int i = 0; i < ItemCount; i++)
+            {
+                if (Orientation == Horizontal)
+                {
+                    MakeMeasureSpec(recycler, i,
+                            View.MeasureSpec.MakeMeasureSpec(i, MeasureSpecMode.Unspecified),
+                            heightSpec,
+                            _mMeasuredDimension);
+
+                    width = width + _mMeasuredDimension[0];
+                    if (i == 0)
+                    {
+                        height = _mMeasuredDimension[1];
+                    }
+                }
+                else
+                {
+                    MakeMeasureSpec(recycler, i,
+                            widthSpec,
+                            View.MeasureSpec.MakeMeasureSpec(i, MeasureSpecMode.Unspecified),
+                            _mMeasuredDimension);
+                    height = height + _mMeasuredDimension[1];
+                    if (i == 0)
+                    {
+                        width = _mMeasuredDimension[0];
+                    }
+                }
+            }
+            switch (widthMode)
+            {
+                case MeasureSpecMode.Exactly:
+                    width = widthSize;
+                    break;
+                case MeasureSpecMode.AtMost:
+                case MeasureSpecMode.Unspecified:
+                    break;
+            }
+
+            switch (heightMode)
+            {
+                case MeasureSpecMode.Exactly:
+                    height = heightSize;
+                    break;
+                case MeasureSpecMode.AtMost:
+                case MeasureSpecMode.Unspecified:
+                    break;
+            }
+
+            SetMeasuredDimension(width, height);
+
+        }
+
+
+
+        private void MakeMeasureSpec(RecyclerView.Recycler recycler, int position, int widthSpec,
+                                   int heightSpec, int[] measuredDimension)
+        {
+            View view = recycler.GetViewForPosition(position);
+            recycler.BindViewToPosition(view, position);
+            if (view != null)
+            {
+                RecyclerView.LayoutParams p = (RecyclerView.LayoutParams)view.LayoutParameters;
+                int childWidthSpec = ViewGroup.GetChildMeasureSpec(widthSpec,
+                        PaddingLeft + PaddingRight, p.Width);
+                int childHeightSpec = ViewGroup.GetChildMeasureSpec(heightSpec,
+                        PaddingTop + PaddingBottom, p.Height);
+                view.Measure(childWidthSpec, childHeightSpec);
+                measuredDimension[0] = view.MeasuredWidth + p.LeftMargin + p.RightMargin;
+                measuredDimension[1] = view.MeasuredHeight + p.BottomMargin + p.TopMargin;
+                recycler.RecycleView(view);
+            }
+        }
+    }
+}
--- AdaaMobile/AdaaMobile/AdaaMobile.Droid/CustomRenderers/HorizontalListViewRenderer.cs
+++ AdaaMobile/AdaaMobile/AdaaMobile.Droid/CustomRenderers/HorizontalListViewRenderer.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using AdaaMobile.Controls;
 using AdaaMobile.Droid.CustomRenderers;
+using AdaaMobile.Models;
 using Android.App;
 using Android.Content;
 using Android.Graphics.Drawables;
@@ -19,25 +20,53 @@ using Xamarin.Forms;
 using Xamarin.Forms.Platform.Android;
 using View = Android.Views.View;
 
-[assembly:ExportRenderer(typeof(HorizontalListView), typeof(HorizontalListViewRenderer))]
+[assembly: ExportRenderer(typeof(HorizontalListView), typeof(HorizontalListViewRenderer))]
 namespace AdaaMobile.Droid.CustomRenderers
 {
     public class HorizontalListViewRenderer : ViewRenderer<HorizontalListView, RecyclerView>
     {
         private RecyclerView _recyclerView;
-        private RecyclerView.LayoutManager _layoutManager;
+        private HorizontalListLayoutManager _layoutManager;
 
         protected override void OnElementChanged(ElementChangedEventArgs<HorizontalListView> e)
         {
             base.OnElementChanged(e);
 
             _recyclerView = new RecyclerView(Xamarin.Forms.Forms.Context);
-            _layoutManager = new LinearLayoutManager(Xamarin.Forms.Forms.Context, LinearLayoutManager.Horizontal, false);
+            _layoutManager = new HorizontalListLayoutManager(Xamarin.Forms.Forms.Context, LinearLayoutManager.Horizontal, false);
             _recyclerView.SetLayoutManager(_layoutManager);
+            //_recyclerView.SetMinimumHeight(100);
             _recyclerView.HasFixedSize = false;
             _recyclerView.SetAdapter(DataSource);
-            
+            _recyclerView.Background = new ColorDrawable(Color.Red.ToAndroid());
+            _recyclerView.LayoutChange += _recyclerView_LayoutChange;
             base.SetNativeControl(_recyclerView);
+
+        }
+
+        protected override void OnSizeChanged(int w, int h, int oldw, int oldh)
+        {
+            base.OnSizeChanged(w, h, oldw, oldh);
+        }
+
+        protected override void OnLayout(bool changed, int l, int t, int r, int b)
+        {
+            base.OnLayout(changed, l, t, r, b);
+        }
+
+        private void _recyclerView_LayoutChange(object sender, LayoutChangeEventArgs e)
+        {
+            //TODO:Test to make sure it doesn't make performance issues
+            if (_layoutManager != null)
+            {
+                //This the is the only way I find so far to update the size in Xamarin forms
+                //when list height is auto and not hardwired.
+                int height = _layoutManager.GetMeasuredHeight();
+                if (height > 0)
+                {
+                    Element.HeightRequest = DpToPixel(height);
+                }
+            }
         }
 
 
@@ -90,33 +119,65 @@ namespace AdaaMobile.Droid.CustomRenderers
 
         private RecyclerView.ViewHolder OnCreateViewHolder(ViewGroup parent, int viewType)
         {
+
             //var viewCellBinded = (Element.ItemTemplate.CreateContent() as ViewCell);
             //System.Diagnostics.Debug.Assert(viewCellBinded != null, "viewCellBinded != null");
+            //viewCellBinded.BindingContext = new DayWrapper(DateTime.Now) { };
             //var renderer = Platform.CreateRenderer(viewCellBinded.View);
-            //renderer.ViewGroup.SetBackgroundColor(global::Android.Graphics.Color.Blue);
+            ////renderer.ViewGroup.LayoutParameters = new LayoutParams(100, 100);
+            ////renderer.ViewGroup.SetBackgroundColor(global::Android.Graphics.Color.Blue);
             //return new HorizontalListViewHolder(renderer.ViewGroup, renderer);
             var tempView = new TextView(Xamarin.Forms.Forms.Context);
             tempView.Text = "Test";
-            tempView.Background= new ColorDrawable(Color.Aqua.ToAndroid());
-            tempView.SetWidth(100);
-            tempView.SetHeight(100);
+            tempView.Background = new ColorDrawable(Color.Aqua.ToAndroid());
             return new HorizontalListViewHolder(tempView);
         }
 
         public void OnBindViewHolder(RecyclerView.ViewHolder holder, int position)
         {
             //var bindedHolder = (HorizontalListViewHolder)holder;
-            //var item = this.Element.ItemsSource.Cast<object>().ElementAt(position);
+            object item;
+            if (Element.ItemsSource is IList)
+            {
+                item = (Element.ItemsSource as IList)[position];
+            }
+            else
+            {
+                item = this.Element.ItemsSource.Cast<object>().ElementAt(position);
+            }
             //bindedHolder.Renderer.Element.BindingContext = item;
         }
 
         public int GetItemsCount()
         {
-            var collection = this.Element.ItemsSource as ICollection;
+            var collection = this.Element.ItemsSource as IList;
             if (collection != null) return collection.Count;
 
             return Element.ItemsSource.Cast<object>().Count();
         }
+
+        /// <summary>
+        /// http://stackoverflow.com/questions/24465513/how-to-get-detect-screen-size-in-xamarin-forms
+        /// </summary>
+        /// <param name="pixel"></param>
+        /// <returns></returns>
+        private double PixelToDp(double pixel)
+        {
+            var scale = Resources.DisplayMetrics.Density;
+            return (double)((pixel * scale) + 0.5f);
+        }
+
+        /// <summary>
+        /// Returns pixel to be used inside xamarin forms
+        /// </summary>
+        /// <param name="dp"></param>
+        /// <returns></returns>
+        private double DpToPixel(double dp)
+        {
+            var scale = Resources.DisplayMetrics.Density;
+            return (double)((dp - 0.5f) / scale);
+        }
+
     }
 
 
@@ -171,5 +232,6 @@ namespace AdaaMobile.Droid.CustomRenderers
             Renderer = rendererBinded;
         }
     }
+
 
 }
