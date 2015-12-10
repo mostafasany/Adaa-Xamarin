7cfbe6e734079d8f2e97c02efb1b4416350dcd27
Mostafa <mostafa.elabady@gmail.com>
2015-12-10 12:21:50 +0200

    no message

---
+++ AdaaMobile/AdaaMobile/AdaaMobile.Droid/CustomRenderers/CustomLabelRenderer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reflection.Emit;
+using AdaaMobile.Droid;
+using Android.Graphics;
+using Android.Widget;
+using Xamarin.Forms.Platform.Android;
+using Xamarin.Forms;
+
+[assembly: ExportRenderer (typeof (Xamarin.Forms.Label), typeof (CustomLabelRenderer))]
+
+namespace AdaaMobile.Droid
+{
+	public class CustomLabelRenderer : LabelRenderer {
+		protected override void OnElementChanged (ElementChangedEventArgs<Xamarin.Forms.Label> e) {
+			base.OnElementChanged (e);
+			var label = (TextView)Control; // for example
+			Typeface font = Typeface.CreateFromAsset (Forms.Context.Assets, "ProximaNova-Regular.otf");
+			label.Typeface = font;
+		}
+	}
+}
+}
+
