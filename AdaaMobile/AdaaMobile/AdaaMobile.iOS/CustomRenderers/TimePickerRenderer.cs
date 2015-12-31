96ed6fa0cf342ce5d2f5f94d8f316cbd351b0972
Mostafa El-Abady <mostafa.elabady@gmail.com>
2015-12-31 10:56:39 +0200

    ios changes

---
+++ AdaaMobile/AdaaMobile/AdaaMobile.iOS/CustomRenderers/TimePickerRenderer.cs
@@ -0,0 +1,21 @@
+﻿using AdaaMobile.iOS.CustomRenderers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(TimePicker), typeof(CustomTimePickerRenderer))]
+
+namespace AdaaMobile.iOS.CustomRenderers
+{
+   public class CustomTimePickerRenderer: TimePickerRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<TimePicker> e)
+        {
+            base.OnElementChanged(e);
+            Control.Layer.BorderColor = UIColor.White.CGColor;
+        }
+    }
+}
