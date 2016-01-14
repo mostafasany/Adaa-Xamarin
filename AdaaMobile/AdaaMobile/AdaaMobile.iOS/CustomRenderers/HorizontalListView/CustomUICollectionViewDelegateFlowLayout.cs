2784468147393095714feb2c80b320160d8f8bd4
Mostafa <mostafa.elabady@gmail.com>
2016-01-14 13:29:29 +0200

    no message

---
+++ AdaaMobile/AdaaMobile/AdaaMobile.iOS/CustomRenderers/HorizontalListView/CustomUICollectionViewDelegateFlowLayout.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CoreGraphics;
+using Foundation;
+using UIKit;
+
+
+namespace AdaaMobile.iOS.CustomRenderers.HorizontalListView
+{
+	public class CustomUICollectionViewDelegateFlowLayout: UICollectionViewDelegateFlowLayout
+	{
+
+		//        public override CGSize GetSizeForItem(UICollectionView collectionView, UICollectionViewLayout layout, NSIndexPath indexPath)
+		//        {
+		//            CGSize baseSize = base.GetSizeForItem(collectionView, layout, indexPath);
+		//
+		//            CGSize size = new CGSize(200f, 200f);
+		//            return baseSize;
+		//        }
+	}
+}
