a7f02eea8875fcbd513f1bc8473417ae51eaf087
ahmedrashad <arashadm@outlook.com>
2016-01-14 16:41:45 +0200

    -Updating packages

--- AdaaMobile/AdaaMobile/AdaaMobile/Helpers/AppSettings.cs
+++ AdaaMobile/AdaaMobile/AdaaMobile/Helpers/AppSettings.cs
@@ -4,7 +4,7 @@ using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Refractored.Xam.Settings;
+using Plugin.Settings;
 using Xamarin.Forms;
 
 namespace AdaaMobile.Helpers
