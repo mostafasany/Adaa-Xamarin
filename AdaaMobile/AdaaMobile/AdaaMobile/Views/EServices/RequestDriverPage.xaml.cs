60a8b39c805bf1b972639848e6569e201f61cb03
Mostafa El-Abady <mostafa.elabady@gmail.com>
2016-03-12 15:50:18 +0200

    Driver source and destination changes

--- AdaaMobile/AdaaMobile/AdaaMobile/Views/EServices/RequestDriverPage.xaml.cs
+++ AdaaMobile/AdaaMobile/AdaaMobile/Views/EServices/RequestDriverPage.xaml.cs
@@ -5,24 +5,24 @@ using Xamarin.Forms;
 
 namespace AdaaMobile.Views.EServices
 {
-	public partial class RequestDriverPage : ContentPage
-	{
-		RequestDriverViewModel _viewModel;
+    public partial class RequestDriverPage : ContentPage
+    {
+        RequestDriverViewModel _viewModel;
 
-		public RequestDriverPage ()
-		{
-			InitializeComponent ();
-			NavigationPage.SetBackButtonTitle(this, "");
+        public RequestDriverPage()
+        {
+            InitializeComponent();
+            NavigationPage.SetBackButtonTitle(this, "");
 
-			_viewModel = Locator.Default.RequestDriverViewModel;
-			BindingContext = _viewModel;
+            _viewModel = Locator.Default.RequestDriverViewModel;
+            BindingContext = _viewModel;
 
-			Title = AppResources.RequestDriver;
+            Title = AppResources.RequestDriver;
 
-			//Initialize picker and wire events
-			ReasonTypePicker.Items.Add(AppResources.Work);
-			ReasonTypePicker.Items.Add(AppResources.Personal);
-			ReasonTypePicker.SelectedIndexChanged += ReasonTypePicker_SelectedIndexChanged;
+            //Initialize picker and wire events
+            ReasonTypePicker.Items.Add(AppResources.Work);
+            ReasonTypePicker.Items.Add(AppResources.Personal);
+            ReasonTypePicker.SelectedIndexChanged += ReasonTypePicker_SelectedIndexChanged;
 
             PriorityPicker.Items.Add(AppResources.Normal);
             PriorityPicker.Items.Add(AppResources.Meduim);
@@ -31,50 +31,64 @@ namespace AdaaMobile.Views.EServices
 
             //Add submit action
             Action action = () =>
-			{
-				_viewModel.NewDriverRequestCommand.Execute(null);
-			};
-			ToolbarItems.Add(
-				new ToolbarItem("", "right.png", action, ToolbarItemOrder.Primary));
-		}
+            {
+                _viewModel.NewDriverRequestCommand.Execute(null);
+            };
+            ToolbarItems.Add(
+                new ToolbarItem("", "right.png", action, ToolbarItemOrder.Primary));
+        }
 
-		private void Priority_Tapped(object sender, EventArgs e)
-		{
-			PriorityPicker.Unfocus();
-			PriorityPicker.Focus();
-		}
+        private void Priority_Tapped(object sender, EventArgs e)
+        {
+            PriorityPicker.Unfocus();
+            PriorityPicker.Focus();
+        }
 
-		private void ReasonType_Tapped(object sender, EventArgs e)
-		{
-			ReasonTypePicker.Unfocus();
-			ReasonTypePicker.Focus();
-		}
+        private void ReasonType_Tapped(object sender, EventArgs e)
+        {
+            ReasonTypePicker.Unfocus();
+            ReasonTypePicker.Focus();
+        }
 
-		private void RequestDate_Tapped(object sender, EventArgs e)
-		{
-			RequestDatePicker.Unfocus();
-			RequestDatePicker.Focus();
-		}
 
-		private void RequestTimeSpan_Tapped(object sender, EventArgs e)
-		{
-			RequestTimePicker.Unfocus();
-			RequestTimePicker.Focus();
-		}
+        private void SourceText_Tapped(object sender, EventArgs e)
+        {
+            SourcePicker.Unfocus();
+            SourcePicker.Focus();
+        }
 
-		private void ReasonTypePicker_SelectedIndexChanged(object sender, EventArgs e)
-		{
-			if (ReasonTypePicker.SelectedIndex == 0)
-			{
-				_viewModel.ReasonType = "Work";
-				_viewModel.LocalizedReasonType = AppResources.Work;
-			}
-			else
-			{
-				_viewModel.ReasonType = "Personal";
-				_viewModel.LocalizedReasonType = AppResources.Personal;
-			}
-		}
+
+        private void Destination_Tapped(object sender, EventArgs e)
+        {
+            DestinationPicker.Unfocus();
+            DestinationPicker.Focus();
+        }
+
+        private void RequestDate_Tapped(object sender, EventArgs e)
+        {
+            RequestDatePicker.Unfocus();
+            RequestDatePicker.Focus();
+        }
+
+        private void RequestTimeSpan_Tapped(object sender, EventArgs e)
+        {
+            RequestTimePicker.Unfocus();
+            RequestTimePicker.Focus();
+        }
+
+        private void ReasonTypePicker_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (ReasonTypePicker.SelectedIndex == 0)
+            {
+                _viewModel.ReasonType = "Work";
+                _viewModel.LocalizedReasonType = AppResources.Work;
+            }
+            else
+            {
+                _viewModel.ReasonType = "Personal";
+                _viewModel.LocalizedReasonType = AppResources.Personal;
+            }
+        }
 
         private void PriorityPicker_SelectedIndexChanged(object sender, EventArgs e)
         {
