01814375c56d71463b273b9202a1ef9aaf917098
mostafasany <mostafasany@MOSTAFA>
2016-08-06 14:06:31 +0300

    Pending Taks

--- AdaaMobile/AdaaMobile/AdaaMobile/ViewModels/TimeSheet/MyPendingTasksViewModel.cs
+++ AdaaMobile/AdaaMobile/AdaaMobile/ViewModels/TimeSheet/MyPendingTasksViewModel.cs
@@ -9,51 +9,62 @@ using AdaaMobile.DataServices.Requests;
 
 namespace AdaaMobile.ViewModels
 {
-	public class MyPendingTasksViewModel : BindableBase
-	{
-		#region Fields
+    public class MyPendingTasksViewModel : BindableBase
+    {
+        #region Fields
 
-		private readonly INavigationService _navigationService;
-		private readonly IDataService _dataService;
+        private readonly INavigationService _navigationService;
+        private readonly IDataService _dataService;
 
-		#endregion
+        #endregion
 
-		#region Properties
+        #region Properties
 
-		private List<PendingTask> _PendingTask;
+        private List<PendingTask> _PendingTask;
 
-		public List<PendingTask> PendingTask {
-			get { return _PendingTask; }
-			set { SetProperty (ref _PendingTask, value); }
-		}
+        public List<PendingTask> PendingTask
+        {
+            get { return _PendingTask; }
+            set { SetProperty(ref _PendingTask, value); }
+        }
 
-		private bool _IsBusy;
+        private PendingTask _SelectedPendingTask;
 
-		public bool IsBusy {
-			get { return _IsBusy; }
-			set { 
-				SetProperty (ref _IsBusy, value); 
-			}
-		}
+        public PendingTask SelectedPendingTask
+        {
+            get { return _SelectedPendingTask; }
+            set { SetProperty(ref _SelectedPendingTask, value); }
+        }
+
+        private bool _IsBusy;
+
+        public bool IsBusy
+        {
+            get { return _IsBusy; }
+            set
+            {
+                SetProperty(ref _IsBusy, value);
+            }
+        }
 
 
-		#endregion
+        #endregion
 
-		#region Initialization
+        #region Initialization
 
-		public MyPendingTasksViewModel(INavigationService navigationService, IDataService dataservice)
-		{
-			_navigationService = navigationService;
-			_dataService = dataservice;
-			PageLoadedCommand = new AsyncExtendedCommand (Loaded);
+        public MyPendingTasksViewModel(INavigationService navigationService, IDataService dataservice)
+        {
+            _navigationService = navigationService;
+            _dataService = dataservice;
+            PageLoadedCommand = new AsyncExtendedCommand(Loaded);
             RequestItemSelectedCommand = new AsyncExtendedCommand<PendingTask>(OpenRequestDetailsPage);
         }
 
-		#endregion
+        #endregion
 
-		#region Commands
+        #region Commands
 
-		public AsyncExtendedCommand PageLoadedCommand { get; set; }
+        public AsyncExtendedCommand PageLoadedCommand { get; set; }
 
         public AsyncExtendedCommand<PendingTask> RequestItemSelectedCommand { get; set; }
 
@@ -61,30 +72,39 @@ namespace AdaaMobile.ViewModels
 
         #region Methods
 
-        private async Task Loaded ()
-		{
-			try {
-				IsBusy = true;
-				var response = await _dataService.GetPendingTaskAsync (null);
-				if (response.ResponseStatus == ResponseStatus.SuccessWithResult && response.Result != null) {
-					if (response.Result != null && response.Result.Count > 0) {
-						PendingTask = response.Result;
-					} else {
+        private async Task Loaded()
+        {
+            try
+            {
+                IsBusy = true;
+                var response = await _dataService.GetPendingTaskAsync(null);
+                if (response.ResponseStatus == ResponseStatus.SuccessWithResult && response.Result != null)
+                {
+                    if (response.Result != null && response.Result.Count > 0)
+                    {
+                        PendingTask = response.Result;
+                    }
+                    else
+                    {
 
-					}
-				}
+                    }
+                }
 
-			} catch (Exception ex) {
-				
-			} finally {
-				IsBusy = false;
-			}
+            }
+            catch (Exception ex)
+            {
 
-		}
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+
+        }
 
         private async Task OpenRequestDetailsPage(PendingTask pendingTask)
         {
-            //SelectedRequest = selectedRequest;
+            SelectedPendingTask = pendingTask;
             //_navigationService.NavigateToPage(typeof(SelectedRequestPage));
         }
 
