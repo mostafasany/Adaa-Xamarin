17a32bcb229806010b7466c2cb51207020ddabd2
mostafasany <mostafasany@MOSTAFA>
2016-08-06 18:58:51 +0300

    Refactoring TimeSheet

--- AdaaMobile/AdaaMobile/AdaaMobile/Views/TimeSheet/MyTimesheetPage.xaml.cs
+++ AdaaMobile/AdaaMobile/AdaaMobile/Views/TimeSheet/MyTimesheetPage.xaml.cs
@@ -1,66 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 using AdaaMobile.ViewModels;
 using AdaaMobile.Strings;
-using AdaaMobile.Views.TimeSheet;
-
 namespace AdaaMobile
 {
 
-	public partial class MyTimesheetPage : ContentPage
-	{
-		private readonly MyTimeSheetViewModel _viewModel;
-		public MyTimesheetPage ()
-		{
-			InitializeComponent ();
-			NavigationPage.SetBackButtonTitle(this, "");
+    public partial class MyTimesheetPage : ContentPage
+    {
+        private readonly MyTimeSheetViewModel _viewModel;
+        public MyTimesheetPage()
+        {
+            InitializeComponent();
+            NavigationPage.SetBackButtonTitle(this, "");
 
-			_viewModel = Locator.Default.MyTimeSheetViewModel;
-			BindingContext = _viewModel;
+            _viewModel = Locator.Default.MyTimeSheetViewModel;
+            BindingContext = _viewModel;
 
-			Title = AppResources.EServices;
+            Title = AppResources.TimeSheet_MyTimeSheet;
+        }
 
-			LoadDuration ();
-		}
-
-		protected override void OnAppearing ()
-		{
-			base.OnAppearing ();
-			_viewModel.PageLoadedCommand.Execute (null);
-		}
-
-		private void LoadDuration()
-		{
-			DurationPicker.Items.Clear ();
-			for (int i = 0; i < 24; i++) {
-				string value = i.ToString () + ":00";
-				DurationPicker.Items.Add(value);
-				value = i.ToString () + ":30";
-				DurationPicker.Items.Add(value);
-			}
-			DurationPicker.SelectedIndexChanged += DurationPicker_SelectionIndexChanged;
-		}
-
-		private void Duration_OnClicked (object sender, EventArgs e)
-		{
-			DurationPicker.Unfocus ();
-			DurationPicker.Focus ();
-		}
-
-		private void SelectTask_OnClicked (object sender, EventArgs e)
-		{
-			Locator.Default.NavigationService.NavigateToPage (typeof(SelectTask));
-		}
-
-		private void DurationPicker_SelectionIndexChanged (object sender, EventArgs e)
-		{
-			if (sender != null && sender is Picker) {
-				var picker = sender as Picker;
-				_viewModel.SelectedDuration = DurationPicker.Items[picker.SelectedIndex];
-			}
-		}
-	}
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _viewModel.PageLoadedCommand.Execute(null);
+        }
+    }
 }
 
