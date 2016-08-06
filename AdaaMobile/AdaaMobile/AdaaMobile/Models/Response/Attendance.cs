b5fa79935eed8fd153fa3e19fc3b608562559f0c
Ahmed Salaheldin <ahmed.salaheldin@itworx.com>
2016-08-06 14:41:03 +0300

    Fixing Bugs in selected Assigment

--- AdaaMobile/AdaaMobile/AdaaMobile/Models/Response/Attendance.cs
+++ AdaaMobile/AdaaMobile/AdaaMobile/Models/Response/Attendance.cs
@@ -5,26 +5,32 @@ using AdaaMobile.ViewModels;
 namespace AdaaMobile.Models.Response
 {
 
-    [System.Xml.Serialization.XmlRoot(ElementName = "root", Namespace = "", IsNullable = false)]
+	[System.Xml.Serialization.XmlRoot (ElementName = "root", Namespace = "", IsNullable = false)]
 	public partial class Assignment
-    {
-        public string Id { get; set; }
+	{
+		public string Id { get; set; }
 
-        public string Title { get; set; }
+		public string Title { get; set; }
 
 		public string Year { get; set; }
 
 		public DateTime StartDate { get; set; }
 
-		public string FinishDate { get; set; }
+		public DateTime FinishDate { get; set; }
 
-		public string AssignmentStatusCode { get{
+		public string FinishDateFormated { get { return FinishDate.Date.ToString("yyyy MMMMM dd"); } }
+
+		public string StartDateFormated { get { return StartDate.Date.ToString("yyyy MMMMM dd"); } }
+
+		public string AssignmentStatusCode {
+			get {
 				if (Locator.Default.AppSettings.SelectedCultureName.Contains ("ar")) {
 					return AssignmentStatusNameAR;
 				} else {
 					return AssignmentStatusNameEN;
 				}
-			} }
+			}
+		}
 
 		public string AssignmentStatusNameAR { get; set; }
 
@@ -32,7 +38,7 @@ namespace AdaaMobile.Models.Response
 
 
 
-    }
+	}
 
 
 }
