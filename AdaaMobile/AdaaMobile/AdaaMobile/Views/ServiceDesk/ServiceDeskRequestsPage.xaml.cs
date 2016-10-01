3de544e472c9dba03f4b99ea7f79a29c45ff9b83
Ahmed Salaheldin <ahmed.salaheldin@itworx.com>
2016-10-01 15:41:16 +0200

    adding search and filter in requests

--- AdaaMobile/AdaaMobile/AdaaMobile/Views/ServiceDesk/ServiceDeskRequestsPage.xaml.cs
+++ AdaaMobile/AdaaMobile/AdaaMobile/Views/ServiceDesk/ServiceDeskRequestsPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using AdaaMobile.Strings;
+﻿using System;
+using System.Collections.Generic;
+using AdaaMobile.Strings;
 using AdaaMobile.ViewModels;
 using Xamarin.Forms;
 
@@ -17,8 +19,61 @@ namespace AdaaMobile
 
 			Title = AppResources.ServiceDesk_MyRequests;
 
+			Action action = () =>
+		{
+			filter();
+		};
+			ToolbarItems.Add(
+				new ToolbarItem("", "right.png", action, ToolbarItemOrder.Primary));
+			RequestSearchBar.TextChanged += RequestSearchBar_TextChanged;
+
+		}
+		private void RequestSearchBar_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			(BindingContext as ServiceDeskRequestsViewModel).searchBy(RequestSearchBar.Text);
+
+		}
+		void filter()
+		{
+			FilterPicker.Unfocus();
+			FilterPicker.Focus();
+
+			if (FilterPicker.SelectedIndex < 0)
+			{
+				FilterPicker.Items.Clear();
+				List<string> optionList = new List<string> { "All", "Active", "Canceled", "Closed", "Resolved"};
+
+				for (int i = 0; i < optionList.Count; i++)
+				{
+					FilterPicker.Items.Add(optionList[i]);
+				}
+
+				FilterPicker.SelectedIndexChanged += FilterPicker_SelectionIndexChanged;
+			}
+
 		}
 
+		void FilterPicker_SelectionIndexChanged(object sender, EventArgs e)
+		{
+			string status = "";
+			try
+			{
+				var picker = sender as Picker;
+				if (picker != null && picker.SelectedIndex >= 0)
+				{
+					status = FilterPicker.Items[picker.SelectedIndex];
+
+				}
+			}
+			catch (Exception)
+			{
+			}
+			finally
+			{
+				(BindingContext as ServiceDeskRequestsViewModel).FilterByStatus(status);
+			}
+
+		}
 		protected override void OnAppearing()
 		{
 			base.OnAppearing();
