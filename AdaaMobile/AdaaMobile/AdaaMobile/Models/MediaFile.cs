dfea05fd4009f194af06caaed540e1a16bdd1699
Ahmed Salaheldin <ahmed.salaheldin@itworx.com>
2016-10-05 00:09:36 +0200

    AddingMediaFile

---
+++ AdaaMobile/AdaaMobile/AdaaMobile/Models/MediaFile.cs
@@ -0,0 +1,39 @@
+﻿using Cirrious.MvvmCross.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+
+namespace PP.Core.Models
+{
+    public class PPMediaFile
+    {
+        public Byte[] data { get; set; }
+        public string Extension { get; set; }
+
+        public EventHandler<EventArgs> OnDeleted { get; set; }
+
+
+        public bool isSelected { get; set; }
+
+        private MvxCommand _DeleteCommand;
+        public ICommand DeleteCommand
+        {
+            get
+            {
+                _DeleteCommand = _DeleteCommand ?? new MvxCommand(DoDeleteCommand);
+                return _DeleteCommand;
+            }
+        }
+
+
+        private void DoDeleteCommand()
+        {
+            isSelected = true;
+            //if (OnDeleted != null)
+            //    OnDeleted(this, new EventArgs());
+        }
+
+    }
+}
