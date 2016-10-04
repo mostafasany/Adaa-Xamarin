e4c716a6b43d65491253c753da85efca1f29cd01
Ahmed Salaheldin <ahmed.salaheldin@itworx.com>
2016-10-04 20:21:41 +0200

    add Media Picker

---
+++ AdaaMobile/AdaaMobile/AdaaMobile.iOS/Helpers/MediaPicker.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Threading.Tasks;
+using MonoTouch.UIKit;
+using System.Threading;
+using System.IO;
+using System.Linq;
+using System.Drawing;
+using Xamarin.Forms;
+using PP.Core.Interfaces.Helpers;
+using PP.Core;
+using PP.Core.Models;
+using MonoTouch.Foundation;
+using PP.Core.Localization;
+
+namespace PP.iOS
+{
+	public class MediaPicker :IMediaPicker
+	{
+		AppDelegate appDelegate = UIApplication.SharedApplication.Delegate as AppDelegate;
+		UIImage originalImage;
+		string imageNAme;
+		PPMediaFile result;
+		UIImagePickerController imagePicker;
+		public MediaPicker ()
+		{
+
+		}
+
+		private TaskCompletionSource<PPMediaFile> _completionSource;
+
+		public bool IsCameraAvailable { get; set; }
+
+		public bool IsPhotosSupported { get; set; }
+
+		public bool IsVideosSupported { get; set; }
+
+		public	EventHandler<MediaPickerArgs> OnMediaSelected { get; set; }
+
+
+		public	EventHandler<MediaPickerErrorArgs> OnError { get; set; }
+
+		protected void Handle_FinishedPickingMedia (object sender, UIImagePickerMediaPickedEventArgs e)
+		{
+
+			var tcs = Interlocked.Exchange(ref _completionSource, null);
+			// determine what was selected, video or image
+			bool isImage = false;
+			switch(e.Info[UIImagePickerController.MediaType].ToString()) {
+			case "public.image":
+				isImage = true;
+				break;
+			default:
+				break;
+			}
+
+		
+
+			// if it was an image, get the other image info
+			if (isImage) {
+				// get the original image
+				NSUrl referenceURL = e.Info [new NSString ("UIImagePickerControllerReferenceURL")] as NSUrl;
+				imageNAme = (referenceURL == null) ? "image1.png" : referenceURL.ToString ();
+				originalImage = e.Info [UIImagePickerController.OriginalImage] as UIImage;
+				// dismiss the picker
+
+				PPMediaFile image = new PPMediaFile ();
+				using (NSData imageData = originalImage.AsPNG ()) {
+					Byte[] myimageByteArray = new Byte[imageData.Length];
+					System.Runtime.InteropServices.Marshal.Copy (imageData.Bytes, myimageByteArray, 0, Convert.ToInt32 (imageData.Length));
+
+					image.data = myimageByteArray;
+					image.Extension = imageNAme;
+					result = image;
+					if (OnMediaSelected != null) {
+						OnMediaSelected (this, new MediaPickerArgs (result));
+					}
+					tcs.SetResult (result);
+					imagePicker.DismissViewController (true, null);
+
+					return;
+				}
+			} else {
+				imagePicker.DismissViewController (true,null);
+				iOSDialoge dialog = new iOSDialoge();
+				dialog.Alert(Messages.NotAllowedType, null, Messages.ok);			}
+
+			tcs.SetResult (null);
+		}
+		public  Task<PPMediaFile> SelectPhotoAsync(){
+			imagePicker = new UIImagePickerController ();
+			imagePicker.SourceType = UIImagePickerControllerSourceType.SavedPhotosAlbum;
+			imagePicker.MediaTypes = UIImagePickerController.AvailableMediaTypes (UIImagePickerControllerSourceType.PhotoLibrary);
+			imagePicker.FinishedPickingMedia += Handle_FinishedPickingMedia;
+			imagePicker.Canceled += Handle_Canceled;
+
+			imagePicker.NavigationBar.TintColor = UIColor.White;
+			imagePicker.NavigationBar.TitleTextAttributes =  new UIStringAttributes{ForegroundColor = UIColor.White}; 
+			appDelegate.navigation.PresentViewController(imagePicker, true,null);
+			var ntcs = new TaskCompletionSource<PPMediaFile>();
+			if (Interlocked.CompareExchange(ref _completionSource, ntcs, null) != null)
+				throw new InvalidOperationException("Only one operation can be active at at time");
+
+			return null;
+	
+		}
+
+
+		void Handle_Canceled (object sender, EventArgs e) {
+			imagePicker.DismissViewController(true,null);
+		}
+	}
+}
+
---
+++ AdaaMobile/AdaaMobile/AdaaMobile/Helpers/IMediaPicker.cs
@@ -0,0 +1,92 @@
+﻿using PP.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PP.Core.Interfaces.Helpers
+{
+    /// <summary>
+    /// Interface IMediaPicker
+    /// </summary>
+    public interface IMediaPicker
+    {
+        /// <summary>
+        /// Gets a value indicating whether this instance is camera available.
+        /// </summary>
+        /// <value><c>true</c> if this instance is camera available; otherwise, <c>false</c>.</value>
+        bool IsCameraAvailable { get; }
+        /// <summary>
+        /// Gets a value indicating whether this instance is photos supported.
+        /// </summary>
+        /// <value><c>true</c> if this instance is photos supported; otherwise, <c>false</c>.</value>
+        bool IsPhotosSupported { get; }
+        /// <summary>
+        /// Gets a value indicating whether this instance is videos supported.
+        /// </summary>
+        /// <value><c>true</c> if this instance is videos supported; otherwise, <c>false</c>.</value>
+        bool IsVideosSupported { get; }
+
+        /// <summary>
+        /// Select a picture from library.
+        /// </summary>
+        /// <param name="options">The storage options.</param>
+        /// <returns>Task&lt;IMediaFile&gt;.</returns>
+        Task<PPMediaFile> SelectPhotoAsync();
+
+        /// <summary>
+        /// Event the fires when media has been selected
+        /// </summary>
+        /// <value>The on photo selected.</value>
+        EventHandler<MediaPickerArgs> OnMediaSelected { get; set; }
+
+        /// <summary>
+        /// Gets or sets the on error.
+        /// </summary>
+        /// <value>The on error.</value>
+        EventHandler<MediaPickerErrorArgs> OnError { get; set; }
+    }
+
+    /// <summary>
+    /// Class MediaPickerArgs.
+    /// </summary>
+    public class MediaPickerArgs : EventArgs
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MediaPickerArgs" /> class.
+        /// </summary>
+        /// <param name="mf">The mf.</param>
+        public MediaPickerArgs(PPMediaFile mf)
+        {
+            MediaFile = mf;
+        }
+
+        /// <summary>
+        /// Gets the media file.
+        /// </summary>
+        /// <value>The media file.</value>
+        public PPMediaFile MediaFile { get; private set; }
+    }
+
+    /// <summary>
+    /// Class MediaPickerErrorArgs.
+    /// </summary>
+    public class MediaPickerErrorArgs : EventArgs
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MediaPickerErrorArgs" /> class.
+        /// </summary>
+        /// <param name="ex">The ex.</param>
+        public MediaPickerErrorArgs(Exception ex)
+        {
+            Error = ex;
+        }
+
+        /// <summary>
+        /// Gets the error.
+        /// </summary>
+        /// <value>The error.</value>
+        public Exception Error { get; private set; }
+    }
+}
